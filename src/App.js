f452d87d84c076ce6cc9cf071ce49e1e8b817d60
Shaqeeb Akhtar <shaqeeb.akhtar@wisdmlabs.com>
2023-02-25 17:43:01 +0530

    initial setup for the project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+const App = () => {
+  return <div className="App">App</div>;
+};
+
+export default App;
